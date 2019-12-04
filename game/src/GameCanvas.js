6d637b324bbb808d1672f26d9fe308893ec6f707
Lopez <lopezsm@plu.edu>
2019-12-04 14:39:20 -0800

    Fixed selection bugs and allowed unselection

--- game/src/GameCanvas.js
+++ game/src/GameCanvas.js
@@ -24,11 +24,11 @@ class GameCanvas extends React.Component {
     this.viewModel = new GameState([bp1, bp2, intersect], [thrl1, btl1]);
     const tempState = this.viewModel.getPointsAndLines();
     this.state = {
-        points: tempState.points,
-        lines: tempState.lines,
-        selectedPoints: [],
-        selectedLines: [],
-        selectedTool: 0
+      points: tempState.points,
+      lines: tempState.lines,
+      selectedPoints: [],
+      selectedLines: [],
+      selectedTool: 0
     };
   }
 
@@ -41,9 +41,9 @@ class GameCanvas extends React.Component {
     var ctx = canvas.getContext("2d"); //drawing object, will be drawing on this
     if (ctx != null) {
       ctx.fillStyle = 'black';
+      ctx.strokeStyle = 'black';
       ctx.lineWidth = 1.5;
       this.state.lines.forEach(line => {
-        console.log(line);
         ctx.beginPath();
         ctx.moveTo(line.p1.x, line.p1.y);
         ctx.lineTo(line.p2.x, line.p2.y);
@@ -59,44 +59,47 @@ class GameCanvas extends React.Component {
         ctx.beginPath();
         ctx.arc(point.x, point.y, 5, 0, 2 * Math.PI, false);
         ctx.fill();
-     })
-     ctx.strokeStyle= 'blue';
-     this.state.selectedLines.forEach(line => {
-     ctx.beginPath();
-     ctx.moveTo(line.p1.x, line.p1.y);
-     ctx.lineTo(line.p2.x, line.p2.y);
-     ctx.stroke();
-     })
+      })
+      ctx.strokeStyle = 'blue';
+      this.state.selectedLines.forEach(line => {
+        ctx.beginPath();
+        ctx.moveTo(line.p1.x, line.p1.y);
+        ctx.lineTo(line.p2.x, line.p2.y);
+        ctx.stroke();
+      })
     }
   }
 
-    userClicked = (e) => {
-        const clickPoint = new Point(e.clientX - this.refs.canvas.offsetLeft, e.clientY - this.refs.canvas.offsetTop);
-        console.log(clickPoint);
-        var closePoints = this.state.points.filter(p => p.distance(clickPoint) < 20);
-        closePoints.sort((p1,p2) => p1.distance(clickPoint) - p2.distance(clickPoint));
+  userClicked = (e) => {
+    const clickPoint = new Point(e.clientX - this.refs.canvas.offsetLeft, e.clientY - this.refs.canvas.offsetTop);
+    var closePoints = this.state.points.filter(p => p.distance(clickPoint) < 20);
+    closePoints.sort((p1, p2) => p1.distance(clickPoint) - p2.distance(clickPoint));
 
-        var closeLines = this.state.lines.filter(l => l.distance(clickPoint) < 20);
-        //SORT closeLines hERE
-        console.log(closeLines);
-        if (closePoints.length > 0) {
-            var sPoints = this.state.selectedPoints;
-            sPoints.push(closePoints[0]);
-            this.setState({selectedPoints: sPoints});
-        } else if (closePoints.length == 0 && closeLines.length > 0) {
-            var sLines = this.state.selectedLines;
-            sLines.push(closeLines[0]);
-            this.setState({selectedLines: sLines});
-        }
-        else {//handles choosing between closeLines and closePoints
-
-        }
+    var closeLines = this.state.lines.filter(l => l.distance(clickPoint) < 20);
+    closeLines.sort((l1,l2) => l1.distance(clickPoint) - l2.distance(clickPoint));
+    if (closePoints.length > 0) {
+      var sPoints = this.state.selectedPoints;
+      if (sPoints.includes(closePoints[0])) {
+        sPoints.splice(sPoints.indexOf(closePoints[0]),1);
+      } else {
+        sPoints.push(closePoints[0]);
+      }
+      this.setState({ selectedPoints: sPoints });
+    } else if (closeLines.length > 0) {
+      var sLines = this.state.selectedLines;
+      if (sLines.includes(closeLines[0])) {
+        sLines.splice(sLines.indexOf(closeLines[0]),1);
+      } else {
+        sLines.push(closeLines[0]);
+      }
+      this.setState({ selectedLines: sLines });
     }
+  }
 
   render() {
     return (
       <div>
-        <canvas ref="canvas" width={640} height={600} onClick={this.userClicked}/>
+        <canvas ref="canvas" width={640} height={600} onClick={this.userClicked} />
         <br />
         <button name="intersectTool">Intersect</button>
         <button name="throughTool">Fold through</button>
